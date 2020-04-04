17f24092a6863a1ecbf5895b79d891a1053cfc93
plindemuth <35471797+plindemuth@users.noreply.github.com>
2020-04-03 21:32:56 -0400

    setup Random Quiz dropdowns to use SQL db

--- app.js
+++ app.js
@@ -13,13 +13,9 @@ app.use('/assets', express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 //start business logic
-
-
-
 var config = {
-    server: 'localhost', 
+    server: 'localhost\\SQLExpress',
     database: 'uTrivia',
     options: {
         trustedConnection: true
@@ -29,37 +25,30 @@ var config = {
 
 app.use(session({secret: 'uncc'})); //initialize session
 
-
-app.get('/', function(req, res){ //index handler
-    res.render('index');
-});
-
 app.get('/about', function (req, res) { //about handler
     res.render('about');
 });
 
 app.get('/quiz', function (req, res) { //quiz handler !IMPORTANT!
-   
+
     sql.connect(config, function (err) {
-    
+
         if (err) console.log(err);
-    
+
         // create Request object
         var request = new sql.Request();
-        
+
         // query to the database and get the records
-        request.query('select TOP 1 * from QUESTIONS', function (err, data) {
+        request.query('select TOP 3 * from QUESTIONS', function (err, data) {
             if (err) console.log(err)
-           // console.log(data.recordset[0].QUESTION);
-           
-           // console.table(data.recordset);
+
             res.render('quiz', {data: data});
-            
+
         });
-        
+
     });
 
-    
+
 });
 
 app.get('/contact', function (req, res) { //contact handler
@@ -68,6 +57,36 @@ app.get('/contact', function (req, res) { //contact handler
 
 app.get('/leaderboards', function (req, res) { //leaderboards handler
     res.render('leaderboards');
+});
+
+app.get('/', function(req, res){ //index handler
+    var categoryList;
+    var difficultyList;
+
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        request.query('select CATEGORY_NAME from CATEGORY ORDER BY CATEGORY_NAME', function (err, data) {
+            if (err) console.log(err)
+
+            categoryList = data.recordset;
+
+            request.query('select LEVEL from DIFFICULTY', function (err, data) {
+                if (err) console.log(err)
+
+                difficultyList = data.recordset;
+
+                console.log(difficultyList);
+                console.log(categoryList);
+                res.render('index', {difficulties: difficultyList, categories: categoryList});
+            });
+        });
+    });
 });
 
 app.listen(3000); //open server
